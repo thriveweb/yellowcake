eca6232f970588e37b64fb02c05f8a370937a9eb
Vincent Weltje <vincent@thriveweb.com.au>
2018-10-17 09:43:03 +1000

    updated our image handling file

--- src/components/Image.js
+++ src/components/Image.js
@@ -1,21 +1,7 @@
 import React from 'react'
-import GatsbyImage from 'gatsby-image'
-import { graphql } from 'gatsby'
 import PropTypes from 'prop-types'
-import _get from 'lodash/get'
 
 import './Image.css'
-
-const extractChildImageSharp = (src = '', format) => {
-  if (!format) {
-    if (typeof src === 'string' && !format) return src
-    const childImageSharp = _get(src, 'childImageSharp')
-    if (!childImageSharp) return _get(src, 'publicURL')
-  }
-  if (format === 'sizes' || format === 'resolutions')
-    return _get(src, `childImageSharp.${format}`)
-  return src
-}
 
 class Image extends React.Component {
   render() {
@@ -24,17 +10,12 @@ class Image extends React.Component {
       backgroundSize = 'cover',
       className = '',
       src,
-      source,
       onClick,
       sizes,
-      alt,
-      style,
-      imgStyle
+      alt
     } = this.props
 
-    const imageSizes = extractChildImageSharp(src, 'sizes')
-    const resolutions = extractChildImageSharp(src, 'resolutions')
-    const imageSrc = extractChildImageSharp(src || source)
+    const imageSrc = src
 
     if (background) {
       let style = {}
@@ -63,20 +44,6 @@ class Image extends React.Component {
       )
     }
 
-    if (imageSizes || resolutions) {
-      return (
-        <GatsbyImage
-          className={`Image ${className}`}
-          sizes={imageSizes}
-          resolutions={resolutions}
-          onClick={onClick}
-          alt={alt}
-          style={style}
-          imgStyle={imgStyle}
-        />
-      )
-    }
-
     return (
       <img
         className={`Image ${className}`}
@@ -94,78 +61,3 @@ Image.propTypes = {
 }
 
 export default Image
-
-export const query = graphql`
-  fragment FluidImage on File {
-    publicURL
-    childImageSharp {
-      sizes(maxWidth: 2800, quality: 75) {
-        ...GatsbyImageSharpSizes_withWebp
-      }
-    }
-  }
-  fragment NoBlurImage on File {
-    publicURL
-    childImageSharp {
-      sizes(maxWidth: 2800, quality: 75) {
-        ...GatsbyImageSharpSizes_withWebp_noBase64
-      }
-    }
-  }
-  fragment TracedImage on File {
-    publicURL
-    childImageSharp {
-      sizes(maxWidth: 2800, quality: 75) {
-        ...GatsbyImageSharpSizes_withWebp_tracedSVG
-      }
-    }
-  }
-  fragment LargeImage on File {
-    publicURL
-    childImageSharp {
-      sizes(maxWidth: 1800, quality: 75) {
-        ...GatsbyImageSharpSizes_withWebp
-      }
-    }
-  }
-  fragment MediumImage on File {
-    publicURL
-    childImageSharp {
-      sizes(maxWidth: 800, quality: 75) {
-        ...GatsbyImageSharpSizes_withWebp
-      }
-    }
-  }
-  fragment SmallImage on File {
-    publicURL
-    childImageSharp {
-      sizes(maxWidth: 400, quality: 75) {
-        ...GatsbyImageSharpSizes_withWebp
-      }
-    }
-  }
-  fragment LargeImageFixed on File {
-    publicURL
-    childImageSharp {
-      resolutions(width: 1800, quality: 75) {
-        ...GatsbyImageSharpResolutions_withWebp
-      }
-    }
-  }
-  fragment MediumImageFixed on File {
-    publicURL
-    childImageSharp {
-      resolutions(width: 800, quality: 75) {
-        ...GatsbyImageSharpResolutions_withWebp
-      }
-    }
-  }
-  fragment SmallImageFixed on File {
-    publicURL
-    childImageSharp {
-      resolutions(width: 400, quality: 75) {
-        ...GatsbyImageSharpResolutions_withWebp
-      }
-    }
-  }
-`
