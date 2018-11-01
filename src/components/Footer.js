c6b6eb3453581b437b61025bc545c72bdd28fbae
Vincent Weltje <vweltje@users.noreply.github.com>
2018-11-01 10:18:15 +1000

    Footer copyright date automatic current year

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -5,7 +5,7 @@ import './Footer.css'
 export default ({ globalSettings, socialSettings, navLinks }) => (
   <footer className='Footer'>
     <div className='container taCenter'>
-      <span>© 2017 All rights reserved.</span>
+      <span>© Copyright {new Date().getFullYear()} All rights reserved.</span>
     </div>
   </footer>
 )
