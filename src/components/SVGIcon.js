92d721ad0e83adf29673677443e805e63ebe054b
Vincent Weltje <vincent@thriveweb.com.au>
2018-11-09 13:52:42 +1000

    removed console log

--- src/components/SVGIcon.js
+++ src/components/SVGIcon.js
@@ -3,7 +3,6 @@ import React from 'react'
 import './SVGIcon.css'
 
 export default ({ src }) => {
-  console.log(src)
   const icon = {
     maskImage: `url(${src})`,
     WebkitMaskImage: `url(${src})`
