978ceebba7b20797e1af269ba9895fd949e38412
Vincent Weltje <vincentweltje@Vincents-iMac.local>
2019-01-21 11:59:35 +1000

    chache instagram feed

--- src/components/InstagramFeed.js
+++ src/components/InstagramFeed.js
@@ -6,6 +6,18 @@ import Image from '../components/Image'
 import './InstagramFeed.css'
 
 export default class InstagramFeed extends Component {
+  constructor(props) {
+    super(props)
+    const lastclear = localStorage.getItem('lastclear'),
+      time_now = new Date().getTime()
+
+    // .getTime() returns milliseconds so 1000 * 60 * 60 * 24 = 1 days
+    if (time_now - lastclear > 1000 * 60 * 60 * 1) {
+      localStorage.clear()
+      localStorage.setItem('lastclear', time_now)
+    }
+  }
+
   static defaultProps = {
     instagramUrl: 'https://instagram.com/instagram',
     count: 7
@@ -34,16 +46,27 @@ export default class InstagramFeed extends Component {
     string.match(/(?:https?:\/\/)(?:www.)?instagram.com\/([\w\d_-]+)\/?/i)
 
   fetchInstagram = instagramUsername => {
-    typeof window !== 'undefined' &&
-      window
-        .fetch(`https://instagram.thrivex.io/?ref=thrivegoldcoast`)
-        .then(res => res.json())
-        .then(data => {
-          this.setState({
-            posts: data && data.items ? data.items : []
+    let insaFeed = localStorage.getItem('insaFeed')
+      ? localStorage.getItem('insaFeed')
+      : false
+
+    if (!insaFeed) {
+      typeof window !== 'undefined' &&
+        window
+          .fetch(`https://instagram.thrivex.io/?ref=thrivegoldcoast`)
+          .then(res => res.json())
+          .then(data => {
+            insaFeed = data && data.items ? data.items : []
+            localStorage.setItem('insaFeed', JSON.stringify(insaFeed))
+            this.setState({
+              posts: insaFeed
+            })
           })
-        })
-        .catch(err => console.error(err))
+          .catch(err => console.error(err))
+    }
+    this.setState({
+      posts: JSON.parse(insaFeed)
+    })
   }
 
   renderLoadingItems = () => (
