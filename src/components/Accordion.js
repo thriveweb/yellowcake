eb0ddcce025078c76074820ee9823e0f70865822
Vincent Weltje <vincentweltje@Vincents-iMac.local>
2019-01-25 17:07:20 +1000

    improved source code of accordeon component

--- src/components/Accordion.js
+++ src/components/Accordion.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import ChevronDown from 'react-feather/dist/icons/chevron-down'
-
+import _kebabCase from 'lodash/kebabCase'
 import './Accordion.css'
 
 export default class Accordion extends React.Component {
@@ -14,8 +14,9 @@ export default class Accordion extends React.Component {
   }
 
   handleClick = index => {
-    const activeItem = this.state.activeItem === index ? null : index
-    this.setState({ activeItem })
+    this.setState({
+      activeItem: this.state.activeItem === index ? null : index
+    })
   }
 
   render() {
@@ -23,28 +24,28 @@ export default class Accordion extends React.Component {
     return (
       <div className={`Accordion ${className}`}>
         {!!items &&
-          items.map((item, index) => {
-            const active = this.state.activeItem === index
-            return (
-              <div
-                className={`Accordion--item ${active ? 'active' : ''}`}
-                key={`accordion-item-${item.title + index}`}
-                onClick={() => this.handleClick(index)}
-              >
-                <h2 className="flex">
-                  <span>{item.title}</span> <ChevronDown />
-                </h2>
-                <div className={'description'}>
-                  {item.description} <br />
-                  {item.link && (
-                    <div href={item.link} className="button">
-                      {item.linkTitle}
-                    </div>
-                  )}
-                </div>
+          items.map((item, index) => (
+            <div
+              className={`Accordion--item ${
+                this.state.activeItem === index ? 'active' : ''
+              }`}
+              key={`accordion-item-${_kebabCase(item.title) + '-' + index}`}
+              onClick={() => this.handleClick(index)}
+            >
+              <h2 className="flex">
+                <span>{item.title}</span>
+                <ChevronDown />
+              </h2>
+              <div className={'description'}>
+                {item.description} <br />
+                {item.link && (
+                  <div href={item.link} className="button">
+                    {item.linkTitle}
+                  </div>
+                )}
               </div>
-            )
-          })}
+            </div>
+          ))}
       </div>
     )
   }
