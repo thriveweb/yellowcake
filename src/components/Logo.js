6af72621077b18b79361313983510bd03e33d5cc
massweb <massweb@outlook.fr>
2020-07-27 10:31:42 +0200

    fgdfg

--- src/components/Logo.js
+++ src/components/Logo.js
@@ -5,7 +5,7 @@ export default () => (
   <div
     className="Logo"
     style={{
-      backgroundImage: `url(/images/logoMassWeb.svg)`
+      backgroundImage: `url(/images/logoMassWeb.png)`
     }}
   />
 )
