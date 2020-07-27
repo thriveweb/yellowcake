1a0daf3097aab355cce8f4854a19df7e5ebbce4c
massweb <massweb@outlook.fr>
2020-07-27 22:13:33 +0200

    logo change

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -49,7 +49,7 @@ export class Navigation extends Component {
             <Logo />
           </Link>
           <div className="Nav--Links">
-            <NavLink to="/">Home</NavLink>
+            <NavLink to="/">Accueil</NavLink>
             <NavLink to="/components/">Components</NavLink>
             <div
               className={`Nav--Group ${
