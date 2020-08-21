73329fb6232c3211abd1edb278a5ae1768f3fceb
massweb <massweb@outlook.fr>
2020-08-21 22:56:13 +0200

    Update Logo.js

--- src/components/Logo.js
+++ src/components/Logo.js
@@ -5,7 +5,7 @@ export default () => (
   <div
     className="Logo"
     style={{
-      backgroundImage: `url(/images/logoMW.png)`
+      backgroundImage: `url(/images/logoMW.svg)`
     }}
   />
 )
