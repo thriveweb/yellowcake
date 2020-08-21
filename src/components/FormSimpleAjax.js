df488e5a7a781aa4f24214fb7348d5197ba7a8e7
massweb <massweb@outlook.fr>
2020-08-21 21:17:28 +0200

    Update FormSimpleAjax.js

--- src/components/FormSimpleAjax.js
+++ src/components/FormSimpleAjax.js
@@ -161,7 +161,7 @@ class Form extends React.Component {
           </label>
           <div
             className="g-recaptcha"
-            data-sitekey="6LfKN3kUAAAAAGIM1CbXmaRZx3LIh_W2twn1tzkA"
+            data-sitekey="6Ld98sEZAAAAAEJceHOEiaidA3lf8w08HKEWTWw-"
           />
           {!!subject && <input type="hidden" name="subject" value={subject} />}
           <input type="hidden" name="form-name" value={name} />
