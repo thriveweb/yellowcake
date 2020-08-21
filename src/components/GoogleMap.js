d705ae10b53189fe2f609ad5621ec138079d579b
massweb <massweb@outlook.fr>
2020-08-21 21:17:31 +0200

    Update GoogleMap.js

--- src/components/GoogleMap.js
+++ src/components/GoogleMap.js
@@ -10,8 +10,8 @@ if (process.env.NETLIFY_MAP_KEY) {
 class GoogleMap extends Component {
   static defaultProps = {
     center: {
-      lat: -28.0914483,
-      lng: 153.4425208
+      lat: 46.982843,
+      lng: 3.143564
     },
     zoom: 14
   }
@@ -25,7 +25,7 @@ class GoogleMap extends Component {
           defaultCenter={this.props.center}
           defaultZoom={this.props.zoom}
         >
-          <Marker lat={-28.0914483} lng={153.4425208} text={'Kreyser Avrora'} />
+          <Marker lat={46.982843} lng={3.143564} text={'MassWeb'} />
         </GoogleMapReact>
       </div>
     )
