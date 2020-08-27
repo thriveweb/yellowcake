4c642249195447ff57b1bf4be7e8ee4552cd493a
massweb <massweb@outlook.fr>
2020-08-27 21:55:41 +0200

    Update HomePage.js

--- src/templates/HomePage.js
+++ src/templates/HomePage.js
@@ -8,6 +8,7 @@ import Layout from '../components/Layout'
 // Export Template for use in CMS preview
 export const HomePageTemplate = ({ title, subtitle, featuredImage, body }) => (
   <main className="Home">
+    {/*}
     <PageHeader
       large
       title={title}
@@ -19,7 +20,23 @@ export const HomePageTemplate = ({ title, subtitle, featuredImage, body }) => (
       <div className="container">
         <Content source={body} />
       </div>
-    </section>
+    </section>*/}
+    <Layout>
+      <Helmet>
+        <title>Site en cours de développement</title>
+      </Helmet>
+      <section className="section thick">
+        <div className="container skinny taCenter">
+          <p>
+            <AlertTriangle size="5rem" />
+          </p>
+          <h1>Site en cours de développement</h1>
+          <p>
+            Le site sera disponible prochainement            
+          </p>
+        </div>
+      </section>
+    </Layout>
   </main>
 )
 
