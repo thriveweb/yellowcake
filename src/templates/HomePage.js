db5b9048efffdc042fd5710f62faf9933c13108d
massweb <massweb@outlook.fr>
2020-08-27 22:08:21 +0200

    aaa

--- src/templates/HomePage.js
+++ src/templates/HomePage.js
@@ -1,7 +1,5 @@
 import React from 'react'
 import { graphql } from 'gatsby'
-import Helmet from 'react-helmet'
-import AlertTriangle from 'react-feather/dist/icons/alert-triangle'
 
 import PageHeader from '../components/PageHeader'
 import Content from '../components/Content'
@@ -10,7 +8,6 @@ import Layout from '../components/Layout'
 // Export Template for use in CMS preview
 export const HomePageTemplate = ({ title, subtitle, featuredImage, body }) => (
   <main className="Home">
-    {/*}
     <PageHeader
       large
       title={title}
@@ -22,23 +19,7 @@ export const HomePageTemplate = ({ title, subtitle, featuredImage, body }) => (
       <div className="container">
         <Content source={body} />
       </div>
-    </section>*/}
-    <Layout>
-      <Helmet>
-        <title>Site en cours de développement</title>
-      </Helmet>
-      <section className="section thick">
-        <div className="container skinny taCenter">
-          <p>
-            <AlertTriangle size="5rem" />
-          </p>
-          <h1>Site en cours de développement</h1>
-          <p>
-            Le site sera disponible prochainement            
-          </p>
-        </div>
-      </section>
-    </Layout>
+    </section>
   </main>
 )
 
