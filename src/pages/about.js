4bfc852f7300a5d1e5811413cca2c5a37d7e300a
massweb <massweb@outlook.fr>
2020-08-27 21:42:43 +0200

    aaa

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,12 +1,12 @@
 import React from "react"
 import { Link } from "gatsby"
 
-const IndexPage = () => (
+const AboutPage = () => (
   <main>
     <h1>About the Author</h1>
     <p>Welcome to my Gatsby site.</p>
-    <Link to="/about/">About</Link>
+    <Link to="/">Accueil</Link>
   </main>
 )
 
-export default IndexPage
+export default AboutPage
