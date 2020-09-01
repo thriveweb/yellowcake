b35b06869139e1240ded8384dca53018ba7d9371
massweb <massweb@outlook.fr>
2020-09-01 18:14:25 +0200

    Update Layout.js

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -74,21 +74,11 @@ export default ({ children, meta, title }) => {
               {...data.settingsYaml}
             />
 
-            {/*<GithubCorner url="https://github.com/massweb58/yellowcake" />
+            {/*<GithubCorner url="https://github.com/massweb58/yellowcake" />*/}
 
             <Nav subNav={subNav} />
-            <Fragment>{children}</Fragment>*/}
-            <section className="section thick">
-              <div className="container skinny taCenter">
-                <p>
-                  <AlertTriangle size="5rem" />
-                </p>
-                <h1>Site en Construction</h1>
-                <p>
-                 
-                </p>
-              </div>
-            </section>
+            <Fragment>{children}</Fragment>
+            
 
             <Footer />
           </Fragment>
