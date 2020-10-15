90e9830594d7103dbc49139d870d7b0cc9f31db6
Alex <alex@Alex.local>
2020-10-15 15:03:01 +1000

    return linter

--- src/components/Accordion.js
+++ src/components/Accordion.js
@@ -3,8 +3,6 @@ import ChevronDown from 'react-feather/dist/icons/chevron-down'
 import _kebabCase from 'lodash/kebabCase'
 import './Accordion.css'
 
-/* eslint-disable */
-
 export default class Accordion extends React.Component {
   static defaultProps = {
     items: [],
