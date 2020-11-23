bd9760564dd3ef3c46e2d4f673358eac8e30edb6
yr.min17@gmail.com <Lily@Lily-MacBook.local>
2020-11-23 20:35:21 +0900

    nav margin changed, about pg

--- content/pages/components.md
+++ content/pages/components.md
@@ -1,7 +1,7 @@
 ---
 template: ComponentsPage
-slug: components
-title: Components Page
+slug: about
+title: About Me
 featuredImage: https://ucarecdn.com/df0dc650-6938-412f-aefb-2019d2349e13/
 subtitle: This is the components page subtitle.
 section1: >-
--- src/components/Nav.js
+++ src/components/Nav.js
@@ -72,7 +72,7 @@ export class Navigation extends Component {
           </Link>
           <div className="Nav--Links">
             <NavLink to="/">Home</NavLink>
-            <NavLink to="/components/">Components</NavLink>
+            <NavLink to="/about/">About</NavLink>
             <div
               className={`Nav--Group ${
                 this.state.activeSubNav === 'posts' ? 'active' : ''
