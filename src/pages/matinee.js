49f7ef4fc1644eb3de5384dc2883d0205b934189
yr.min17@gmail.com <Lily@Lily-MacBook.local>
2020-11-23 23:48:02 +0900

    single prod pg added

---
+++ src/pages/matinee.js
@@ -0,0 +1,27 @@
+import React from 'react'
+
+import pic1 from '../../public/images/17-matinee/pic1.jpg'
+import pic2 from '../../public/images/17-matinee/pic2.jpg'
+import pic3 from '../../public/images/17-matinee/pic3.jpg'
+import pic4 from '../../public/images/17-matinee/pic4.jpg'
+import pic5 from '../../public/images/17-matinee/pic5.jpg'
+import pic6 from '../../public/images/17-matinee/pic6.jpg'
+import pic7 from '../../public/images/17-matinee/pic7.jpg'
+import pic8 from '../../public/images/17-matinee/pic8.jpg'
+import pic9 from '../../public/images/17-matinee/pic9.jpg'
+import pic10 from '../../public/images/17-matinee/pic10.jpg'
+import pic11 from '../../public/images/17-matinee/pic11.jpg'
+import pic12 from '../../public/images/17-matinee/pic12.jpg'
+import pic13 from '../../public/images/17-matinee/2014.jpg'
+import poster1 from '../../public/images/17-matinee/poster1.jpg'
+import poster2 from '../../public/images/17-matinee/poster2.jpg'
+
+const Matinee = () => {
+  return (
+   <header>
+     <h2>Matinee!!</h2>
+   </header> 
+  )
+}
+
+export default Matinee
