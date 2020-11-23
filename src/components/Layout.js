5dc52e5459218c49b0c5dd2f14ed84eafbf87a08
yr.min17@gmail.com <Lily@Lily-MacBook.local>
2020-11-23 20:25:41 +0900

    changed title, removed github link

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -4,7 +4,6 @@ import { StaticQuery, graphql } from 'gatsby'
 import Meta from './Meta'
 import Nav from './Nav'
 import Footer from './Footer'
-import GithubCorner from './GithubCorner'
 
 import 'modern-normalize/modern-normalize.css'
 import './globalStyles.css'
@@ -72,8 +71,6 @@ export default ({ children, meta, title }) => {
               {...meta}
               {...data.settingsYaml}
             />
-
-            <GithubCorner url="https://github.com/thriveweb/yellowcake" />
 
             <Nav subNav={subNav} />
 
