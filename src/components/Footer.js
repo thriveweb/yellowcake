a2bbc7a6d887bc30f02e4c30698204361aa6038d
yr.min17@gmail.com <Lily@Lily-MacBook.local>
2020-11-24 03:23:59 +0900

    rm footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,10 +3,10 @@ import './Footer.css'
 
 export default () => (
   <div>
-    <h2 className="taCenter">
+    {/* <h2 className="taCenter">
       Follow us{' '}
       <a href="https://instagram.com/thrivegoldcoast/">@thrivegoldcoast</a>
-    </h2>
+    </h2> */}
     <br />
     <footer className="footer">
       <div className="container taCenter">
