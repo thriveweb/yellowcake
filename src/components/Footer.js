71b6b2ce0254a20181d8bf492191b7fe6205d50f
Avadhoot Dandekar <avadhoot.dandekar@gmail.com>
2021-01-17 16:04:35 +0000

    Footer.js: updated span 1 in span

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -11,7 +11,7 @@ export default () => (
     <footer className="footer">
       <div className="container taCenter">
         <span>
-          © Copyright {new Date().getFullYear()} All rights reserved. Crafted by{' '}
+          © Avadhoot Dandekar {new Date().getFullYear()} All rights reserved. Crafted by{' '}
           <a href="https://thriveweb.com.au/">Thrive</a>.
         </span>
       </div>
